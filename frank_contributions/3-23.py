126c86148b381d9e5b5fc482a54af7852fe329a9
frankmuk <frank.mukendi@techfield.us>
2019-04-25 12:46:48 -0400

    now files can be saved to s3 and hdfs

--- frank_contributions/3-23.py
+++ frank_contributions/3-23.py
@@ -1,26 +1,23 @@
-import credentials
-import boto3
-import random
-import os
+import subprocess
+import time
 from skpy import SkypeEventLoop, SkypeNewMessageEvent
 class SkypePing(SkypeEventLoop):
-	def __init__(self):
-		super(SkypePing, self).__init__(credentials.username, credentials.password)
-	def onEvent(self, event):
-		if isinstance(event, SkypeNewMessageEvent):#
- \
-			f1 = open("s3.txt","a")
-			f2 = open("s3.txt", "r")
-			f1.write(str(event.msg.content) + '\n')
-			f2.seek(0)
-			if(len(f2.readlines()) > 9) :
-				s3 = boto3.client('s3')
-				bucket_name = 'skypedata'
-				s3.upload_file("s3.txt", bucket_name, "dump/file {}".format(random.randint(0,1000)))
-				os.remove("s3.txt")
-				print("updating s3")
-			print(event.msg)
+        def __init__(self):
+                super(SkypePing, self).__init__(credentials.username, credentials.password)
+        def onEvent(self, event):
+                if isinstance(event, SkypeNewMessageEvent):# \
+                        f1 = open("s3.txt","a")
+                        f2 = open("s3.txt", "r")
+                        f1.write(str(event.msg.content) + '\n')
+                        f2.seek(0)
+                        if(len(f2.readlines()) > 9) :
+                                s3 = boto3.client('s3')
+                                bucket_name = 'skypedata'
+                                file_name = "file{}".format(format(time.time()))
+                                os.rename("s3.txt",file_name)  #rename to avoid "file exists error"
+                                s3.upload_file(file_name, bucket_name, "dump/"+file_name)
+                                subprocess.call(["hdfs", "dfs", "-put", file_name]) #save the file to hdfs
+                                                               os.remove(file_name)
+                                print("------------updating s3-----------------")
 
-
-sp = SkypePing()
-sp.loop()
+                        print(event.msg)
