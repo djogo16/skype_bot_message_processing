18e486101b9735df945759ffabe3aa98bd52359d
frankmuk <frank.mukendi@techfield.us>
2019-04-29 14:02:22 -0400

    added script to schedule loading data into hive

---
+++ frank_contributions/hive_job_scheduler.py
@@ -0,0 +1,7 @@
+from hivebatch import load_hive_data
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+scheduler = BlockingScheduler()
+scheduler.add_job(load_hive_data, 'interval', seconds=1)
+scheduler.start()
+
--- frank_contributions/hivebatch.py
+++ frank_contributions/hivebatch.py
@@ -6,21 +6,24 @@
 '''
 import subprocess
 import re
-skype_files = subprocess.check_output(["hdfs","dfs", "-find","*.tsv" ]).decode("utf-8").rstrip().split("\n")
 
-#open file that stores the time hive was last updated. Insert new data into if new files were added in hdfs since then
-with open("saved_time", "r+") as f:
-        hive_last_update_time = float(f.read().rstrip())
-        print("Last time hive was update: {}".format(hive_last_update_time))
-        files_since_last_update = list(filter(lambda x : float(re.findall("\d+\.\d+", x)[0]) > hive_last_update_time ,skype_files))
-        if(len(files_since_last_update) != 0):
-                for myfile in files_since_last_update:
-                        hql_command = "load data inpath \'" + myfile + "\' into table skype_bot.skype_data;"
-                        subprocess.call(["hive","-e",hql_command])
-                        print("Loaded data from file " + myfile + " into table skype_data")
-                f.seek(0)
-                f.write(str(re.findall("\d+\.\d+",files_since_last_update[-1])[0])) #save last updated time to file
-                f.truncate()
-        else:
-                print("No new file detected. Database up to date\n")
+def load_hive_data():
+	skype_files = subprocess.check_output(["hdfs","dfs", "-find","*.tsv" ]).decode("utf-8").rstrip().split("\n")
+
+	#open file that stores the time hive was last updated. Insert new data into if new files were added in hdfs since then
+	with open("saved_time", "r+") as f:
+        	hive_last_update_time = float(f.read().rstrip())
+        	print("Last time hive was update: {}".format(hive_last_update_time))
+        	files_since_last_update = list(filter(lambda x : float(re.findall("\d+\.\d+", x)[0]) > hive_last_update_time ,skype_files))
+		print("new files: " + files_since_last_update)
+        	if(len(files_since_last_update) != 0):
+        	        for myfile in files_since_last_update:
+        	                hql_command = "load data inpath \'" + myfile + "\' into table skype_bot.skype_data;"
+        	                subprocess.call(["hive","-e",hql_command])
+        	                print("Loaded data from file " + myfile + " into table skype_data")
+        	        f.seek(0)
+        	        f.write(str(re.findall("\d+\.\d+",files_since_last_update[-1])[0])) #save last updated time to file
+        	        f.truncate()
+        	else:
+        	        print("No new file detected. Database up to date\n")
 
