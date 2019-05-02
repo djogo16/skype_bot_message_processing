bc3e36d8977e79acf2101be67af6441fe3477e66
frankmuk <frank.mukendi@techfield.us>
2019-05-02 11:06:38 -0400

    old scheduler

--- frank_contributions/hive_job_scheduler.py
+++ frank_contributions/hive_job_scheduler.py
@@ -2,6 +2,6 @@ from hivebatch import load_hive_data
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 scheduler = BlockingScheduler()
-scheduler.add_job(load_hive_data, 'interval', seconds=1)
+scheduler.add_job(load_hive_data, 'interval', hours=1)
 scheduler.start()
 
